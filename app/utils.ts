d13f375774de04af56b44ea4ed5a5fb3f68da848
Quentin Bellanger <hello@quentin-bellanger.com>
2026-04-19 18:09:11 +0200

    fix: generate author type from zod schema

--- app/utils.ts
+++ app/utils.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 /**
  * Format a date string into a readable date ("17 août 2022")
  * @param {string} dateString
@@ -9,3 +11,16 @@ export function formatDate(dateString: string): string {
     day: "numeric"
   }).format(new Date(dateString));
 }
+
+/**
+ * Define content collection schema with Zod
+ */
+export const postSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  cover: z.string(),
+  date: z.date(),
+  author: z.enum(["Quentin Bellanger", "Florian Deblaise"])
+});
+
+export type PostAuthor = z.infer<typeof postSchema>["author"];
--- content.config.ts
+++ content.config.ts
@@ -1,18 +1,12 @@
 import { defineContentConfig, defineCollection } from "@nuxt/content";
-import { z } from "zod";
+import { postSchema } from "./app/utils";
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: "page",
       source: "**/*.md",
-      schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        cover: z.string(),
-        date: z.date(),
-        author: z.enum(["Quentin Bellanger", "Florian Deblaise"])
-      })
+      schema: postSchema
     })
   }
 });
